8c82aa62343c4747c2f4122ea4342712cbb6e920
ricardosantiagosantander <ricardo.santiago.santander@gmail.com>
2026-06-03 11:08:36 -0500

    lint cicd tests4

--- test/test_calculos.py
+++ test/test_calculos.py
@@ -1,5 +1,5 @@
 import pytest
-from app.calculos import sumar, dividir ,multiplicar
+from app.calculos import sumar, dividir, multiplicar
 
 
 def test_sumar():
@@ -13,6 +13,7 @@ def test_dividir():
 def test_dividir_para_cero():
     with pytest.raises(ValueError):
         dividir(10, 0)
+
 
 def test_miltiplciar():
     assert multiplicar(2, 10) == 20
