df23f6470da5fa3356e4758f9fb75c925dbee61a
ricardosantiagosantander <ricardo.santiago.santander@gmail.com>
2026-06-03 09:46:51 -0500

    primer cambio

---
+++ app/calculos.py
@@ -0,0 +1,8 @@
+def sumar(a, b):
+    return a + b
+
+
+def dividir(a, b):
+    if b == 0:
+        raise ValueError("No se puede dividir para cero")
+    return a / b
---
+++ test/test_calculos.py
@@ -0,0 +1,15 @@
+import pytest
+from app.calculos import sumar, dividir
+
+
+def test_sumar():
+    assert sumar(2, 3) == 5
+
+
+def test_dividir():
+    assert dividir(10, 2) == 5
+
+
+def test_dividir_para_cero():
+    with pytest.raises(ValueError):
+        dividir(10, 0)
