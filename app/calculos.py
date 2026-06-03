94a5bf4633389126bb07a07bf51eb5038508211e
ricardosantiagosantander <ricardo.santiago.santander@gmail.com>
2026-06-03 10:59:24 -0500

    lint cicd tests1

--- app/calculos.py
+++ app/calculos.py
@@ -1,11 +1,8 @@
 def sumar(a, b):
     return a + b
-
-
 def dividir(a, b):
     if b == 0:
         raise ValueError("No se puede dividir para cero")
     return a / b
-
-def multiplicar(a,b):
+def multiplicar(a, b):
     return a*b
